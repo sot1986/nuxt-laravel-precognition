defe118c3aacbe854eb84b59b1a632e6f2689085
Matteo Presot <matteo.presot@gmail.com>
2024-04-04 08:10:16 +0200

    lint code

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,6 +1,6 @@
-import { describe, it, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
-import { setup, $fetch } from '@nuxt/test-utils/e2e'
+import { describe, expect, it } from 'vitest'
+import { $fetch, setup } from '@nuxt/test-utils/e2e'
 
 describe('ssr', async () => {
   await setup({
@@ -9,6 +9,7 @@ describe('ssr', async () => {
 
   it('renders the index page', async () => {
     // Get response to a server-rendered page with `$fetch`.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const html = await $fetch('/')
     expect(html).toContain('<h1>Test</h1>')
   })
